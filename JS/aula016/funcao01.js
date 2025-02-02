f942b5c79923f8ace92b418a205ea77ea4553c65
Tavares_RJ <tavares3252.dt@gmail.com>
2025-02-02 17:26:14 -0300

    Aumento de logica simples

--- JS/aula016/funcao01.js
+++ JS/aula016/funcao01.js
@@ -2,7 +2,8 @@
 
 var n = 7262
 
-if(n % 2 == 0){                         // Dificildade encontrada nesse exercício: Atenção aos símbolos!!!!!
+if(n % 2 == 0){                         
+    // Dificildade encontrada nesse exercício: Atenção aos símbolos!!!!!
     console.log(`PAR!`)
 } else{
     console.log('Ímpar!')
